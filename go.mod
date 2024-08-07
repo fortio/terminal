843ce963971b895b91729bdf5a878e58a81fa46e
Laurent Demailly <ldemailly@gmail.com>
2024-08-07 11:13:58 -0700

    it's a skeletal start, but will move to fortio, and use https://github.com/fortio/cli/pull/38

--- go.mod
+++ go.mod
@@ -2,6 +2,16 @@ module grol.io/terminal
 
 go 1.22.6
 
-require golang.org/x/term v0.23.0
+require (
+	fortio.org/cli v1.8.0
+	fortio.org/log v1.16.0
+	golang.org/x/term v0.23.0
+)
 
-require golang.org/x/sys v0.23.0 // indirect
+require (
+	fortio.org/struct2env v0.4.1 // indirect
+	fortio.org/version v1.0.4 // indirect
+	github.com/kortschak/goroutine v1.1.2 // indirect
+	golang.org/x/crypto/x509roots/fallback v0.0.0-20240626151235-a6a393ffd658 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+)
--- terminal.go
+++ terminal.go
@@ -2,9 +2,40 @@
 package terminal // import "grol.io/terminal"
 
 import (
+	"os"
+
 	"golang.org/x/term"
 )
 
-func IsTerminal(fd int) bool {
-	return term.IsTerminal(fd)
+type Terminal struct {
+	fd       int
+	oldState *term.State
+}
+
+// Open opens stdin as a terminal, do `defer terminal.Close()`
+// to restore the terminal to its original state upon exit.
+func Open() (*Terminal, error) {
+	t := &Terminal{fd: int(os.Stdin.Fd())}
+	if !t.IsTerminal() {
+		return t, nil
+	}
+	var err error
+	t.oldState, err = term.MakeRaw(t.fd)
+	if err != nil {
+		return nil, err
+	}
+	return t, nil
+}
+
+func (t *Terminal) IsTerminal() bool {
+	return term.IsTerminal(t.fd)
+}
+
+func (t *Terminal) Close() error {
+	if t.oldState == nil {
+		return nil
+	}
+	err := term.Restore(t.fd, t.oldState)
+	t.oldState = nil
+	return err
 }
